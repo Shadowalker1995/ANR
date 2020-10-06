6364cc38c5c8c2899b65f05d6cca5f5d5a6cf766
Shadowalker1995 <henry664650770@gmail.com>
2020-10-06 11:08:16 -0600

    test

--- ___sample___test__ANR.sh
+++ ___sample___test__ANR.sh
@@ -6,7 +6,7 @@
 # The saved ARNS model weights should be in ./__saved_models__/[dataset] - ARNS/[dataset]_ANRS_[random_seed].pth
 # E.g. ./__saved_models__/amazon_instant_video - ARNS/amazon_instant_video_ANRS_1337.pth
 python PyTorchTEST.py -d "test" -m "ANR" -e 15 -p 1 -v 19438 -rs 1337 -gpu 0 -vb 1 -sm "test_ANR" -ARL_path "test_ANRS_1337"
-#python PyTorchTEST.py -d "test" -m "ANR" -e 15 -p 1 -v 19438 -rs 1234 -gpu 0 -vb 1 -sm "test_ANR" -ARL_path "test_ANRS_1337"
-#python PyTorchTEST.py -d "test" -m "ANR" -e 15 -p 1 -v 19438 -rs 5678 -gpu 0 -vb 1 -sm "test_ANR" -ARL_path "test_ANRS_1337"
-#python PyTorchTEST.py -d "test" -m "ANR" -e 15 -p 1 -v 19438 -rs 1357 -gpu 0 -vb 1 -sm "test_ANR" -ARL_path "test_ANRS_1337"
-#python PyTorchTEST.py -d "test" -m "ANR" -e 15 -p 1 -v 19438 -rs 2468 -gpu 0 -vb 1 -sm "test_ANR" -ARL_path "test_ANRS_1337"
+python PyTorchTEST.py -d "test" -m "ANR" -e 15 -p 1 -v 19438 -rs 1234 -gpu 0 -vb 1 -sm "test_ANR" -ARL_path "test_ANRS_1337"
+python PyTorchTEST.py -d "test" -m "ANR" -e 15 -p 1 -v 19438 -rs 5678 -gpu 0 -vb 1 -sm "test_ANR" -ARL_path "test_ANRS_1337"
+python PyTorchTEST.py -d "test" -m "ANR" -e 15 -p 1 -v 19438 -rs 1357 -gpu 0 -vb 1 -sm "test_ANR" -ARL_path "test_ANRS_1337"
+python PyTorchTEST.py -d "test" -m "ANR" -e 15 -p 1 -v 19438 -rs 2468 -gpu 0 -vb 1 -sm "test_ANR" -ARL_path "test_ANRS_1337"
